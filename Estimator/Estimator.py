50572338a1a44fd1aba067436942df38833af427
srajit <srajits@kpit.com>
2019-01-28 09:14:14 +0100

    changed Estimator.py

--- Estimator/Estimator.py
+++ Estimator/Estimator.py
@@ -1,9 +1,7 @@
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVR
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_squared_error
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_absolute_error
 
 
 class Estimator:
@@ -14,7 +12,7 @@ class Estimator:
         self.classifier = None
         self.train_test_split()
         self.fit()
-        self.score(self.x_test, self.y_test)
+        self.calculate_metrics(self.x_test, self.y_test)
 
     def train_test_split(self):
         y = self.dataset[self.label]
@@ -27,7 +25,20 @@ class Estimator:
         self.classifier = LinearRegression()
         self.classifier.fit(self.x_train, self.y_train)
 
+    def calculate_metrics(self, x, y):
+        prediction = self.classifier.predict(x)
+        actual = y
+        self.mean_absolute_error(prediction, actual)
+        self.mean_squared_error(prediction, actual)
+        self.score(x, y)
+
+    @staticmethod
+    def mean_absolute_error(prediction, actual):
+        print(f'MEAN ABS ERROR:{(abs(prediction - actual).mean())}')
+
+    @staticmethod
+    def mean_squared_error(prediction, actual):
+        print(f'MEAN SQUARED ERROR:{((prediction - actual)**2).mean()}')
+
     def score(self, x, y):
         print(self.classifier.score(x, y))
-        print(mean_squared_error(y, self.classifier.predict(x)))
-        print(mean_absolute_error(y, self.classifier.predict(x)))
